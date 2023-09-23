f946d6399da64de8fe4edc079c7f26fed8f47217
Daudi Mwanzia <daudimwanzia@gmail.com>
2023-09-23 11:34:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickuplocation) {
+  return Math.abs(pickuplocation - 42);
+}
+function distanceFromHqInFeet(blocks) {
+  let feet = distanceFromHqInBlocks(blocks);
+  return feet * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+  let distanceinblocks = Math.abs(destination - start);
+  return distanceinblocks * 264;
+}
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance > 2500) {
+        return "cannot travel that far";
+    } else if (distance > 2000) {
+        return 25;
+    } else if (distance > 400){
+        return (distance - 400) * 0.02;
+    } else {
+        return 0;
+    }
+}
